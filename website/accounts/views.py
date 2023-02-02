43f8b2e5d9c3b555efc92401bc0a36215470fb1e
Jean Petitclerc <jean.petitclerc@groupepp.com>
2023-02-01 22:13:18 -0500

    Navigation pour login/logout/register, couleurs dans base.html

--- website/accounts/views.py
+++ website/accounts/views.py
@@ -4,25 +4,25 @@ from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
 from django.shortcuts import redirect
 from django.db import IntegrityError
-#from bands import urls
+from .forms import UserCreateForm
 
 
 def register_account(request):
     if request.method == 'GET':
-        return render(request, 'register.html', {'form':UserCreationForm})
+        return render(request, 'register.html', {'form':UserCreateForm})
     else:
         if request.POST['password1'] == request.POST['password2']:
             try:
-                user = User.objects.create_user(request.POST['username'], password= request.POST['password1'])
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'], email=request.POST['email'], first_name=request.POST['first_name'], last_name=request.POST['last_name'])
                 user.save()
                 login(request, user)
                 return redirect('bands:home')
             except IntegrityError:
-                form = UserCreationForm(initial={'username': request.POST['username']})
+                form = UserCreateForm(initial={'username': request.POST['username'], 'first_name': request.POST['first_name'], 'last_name': request.POST['last_name'], 'email': request.POST['email']})
                 return render(request, 'register.html', {'form': form,
                                                          'error': "Ce nom d'usager est déjà pris."})
         else:
-            form = UserCreationForm(initial={'username': request.POST['username']})
+            form = UserCreateForm(initial={'username': request.POST['username'], 'first_name': request.POST['first_name'], 'last_name': request.POST['last_name'], 'email': request.POST['email']})
             return render(request, 'register.html', {'form': form,
                                                      'error': 'Les mots de passe ne concordent pas.'})
 
