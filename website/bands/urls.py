a3e9166dd5cba3b0479c5d1dbec8705e945dc43d
Jean Petitclerc <jean.petitclerc@groupepp.com>
2023-02-06 21:48:18 -0500

    Liste de genres

--- website/bands/urls.py
+++ website/bands/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('country', views.list_countries, name='list_countries'),
     path('country/<int:id>/', views.detail_country, name='detail_country'),
+    path('genre', views.list_genres, name='list_genres'),
+    path('genre/<int:id>/', views.detail_genre, name='detail_genre'),
 ]
--- website/bands/views.py
+++ website/bands/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import Http404, HttpResponse
-from .models import Country
+from .models import Country, Genre
 
 def list_countries(request):
     countries = Country.objects.all()
@@ -17,6 +17,23 @@ def detail_country(request, id):
     return render(request,
                   'bands/country/detail.html',
                   {'country': country})
+
+
+def list_genres(request):
+    genres = Genre.objects.all()
+    return render(request,
+                 'bands/genre/list.html',
+                 {'genres': genres})
+
+
+def detail_genre(request, id):
+    try:
+        genre = Genre.objects.get(id=id)
+    except Genre.DoesNotExist:
+        raise Http404("No Genre found.")
+    return render(request,
+                  'bands/genre/detail.html',
+                  {'genre': genre})
 
 
 def home(request):
